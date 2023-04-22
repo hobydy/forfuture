c0e349f186aad4408a802cbd9e195bca31027fc5
apt-get update <131296775+hobydy@users.noreply.github.com>
2023-04-22 21:48:57 +0300

    Update cryptoinfo.py

--- cryptoinfo.py
+++ cryptoinfo.py
@@ -2,8 +2,8 @@ from binance import Client
 import requests
 from bs4 import BeautifulSoup
 
-api_key = 'Your api key'
-secret_key = 'Your secret key'
+api_key = '1RrTe8IPPSZzgebyxAAgNApqjFWrsotdGGpl0Ah5OlNRwyFqVEL7rQ5QrITE1ADD'
+secret_key = '8CsJdbMIBlTr3dHDpENf5JbsycdJbX9pILMoUm5NYKSqTZ4PI3i7Kb7ZxO9zW8Qg'
 
 client = Client(api_key, secret_key)
 
@@ -18,21 +18,21 @@ eth_price = float(ETHUSDT['price'])
 
 # Достаем цену 1 лея в долларах
 urlusd = 'https://www.google.com/finance/quote/MDL-LTC?sa=X&ved=2ahUKEwjI-IGfu73-AhVKxAIHHSidC_0Q-fUHegQIBhAf'
-r = requests.get(urlusd)
-soup = BeautifulSoup(r.text, 'html.parser')
+requsd = requests.get(urlusd)
+soup = BeautifulSoup(requsd.text, 'html.parser')
 mdlltcget = soup.find('div', class_="YMlKec fxKbKc").text
 mdlltc = float(mdlltcget)
 
 # Достаем цену 1 лея в BTC
 urlbtc = "https://www.google.com/finance/quote/MDL-BTC?sa=X&ved=2ahUKEwjwmOeRib7-AhXy7LsIHQ0TCQEQ-fUHegQIBhAf"
-r = requests.get(urlbtc)
-soup = BeautifulSoup(r.text, "html.parser")
+reqbtc = requests.get(urlbtc)
+soup = BeautifulSoup(reqbtc.text, "html.parser")
 mdlbtcget = soup.find('div', class_='YMlKec fxKbKc').text
 mdlbtc = float(mdlbtcget)
 
 # Достаем цену 1 лея в ETH
 urleth = 'https://www.google.com/finance/quote/MDL-ETH?sa=X&ved=2ahUKEwiL_faTjL7-AhUdgf0HHb54BksQ-fUHegQIDhAf'
-r = requests.get(urleth)
-soup = BeautifulSoup(r.text, "html.parser")
+reqeth = requests.get(urleth)
+soup = BeautifulSoup(reqeth.text, "html.parser")
 mdlethget = soup.find('div', class_='YMlKec fxKbKc').text
 mdleth = float(mdlethget)
