5eaddb8b4a5bea998bbb5202e3becbc450cebb4d
apt-get update <131296775+hobydy@users.noreply.github.com>
2023-05-08 15:20:42 +0300

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -3,10 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 import random
 from TOKEN import TOKEN
 import emoji
-from rulesmsg import rulesmsg
-from cryptoinfo import mdlltc, btc_price, ltc_price, eth_price
-from btcget import mdlbtc
-from ethget import mdleth
+from TgBot.cryptoinfo import mdlltc, mdlbtc, mdleth, btc_price, ltc_price, eth_price
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -17,7 +14,7 @@ dp = Dispatcher(bot)
 async def send_welcome(message: types.Message):
     await message.reply(f"Привет {message.from_user.full_name}\n"
                         f"Я умный помошник.\n"
-                        f"Включаю в себя несколько функций.\n"
+                        f"Включаю в себя некоторые функции.\n"
                         f"Воспользуйся командой /info что увидеть весь список доступнхы функций")
 
 
@@ -35,28 +32,33 @@ async def roll_dice(message: types.Message):
         # Если число не найдено, генерируем случайное число от 1 до 100
         await message.reply('Случайное число от 1 до 100:\n' + str(random.randint(1, 100)))
 
+
 # Обрабатываю команду /info
 @dp.message_handler(commands=['info'])
 async def send_info(message: types.Message):
     await message.reply(f"{message.from_user.full_name} данный бот разработан что бы помочь и облегчить пользование 'Telegram'\n"
-                        f"Все права пренадлежат @xlopnul_c_ytpa\n"
-                        f"Обращайтесь в любоем время по поводу сотрудничества.\n"
+                        f"Не продавая и Не пропагандируя ничего Не законного.\n"
                         f"Список доступных команд:\n"
                         f"/info - Показывает информацию о боте\n"
                         f"/roll - Генерирует любое случайное число в заданом деапазоне\n"
                         f"/rules - Правила чата Цитадель\n"
-                        f"Исходный код бота вы можете найти тут:\n"
-                        f"https://github.com/hobydy/forfuture\n"
-                        f"/mdlltc - Пишем команду указав сразу сумму желаймого обмена в леях '/mdlltc 500'\n"
-                        f"/mdlbtc - Пишем команду указав сразу сумму желаймого обмена в леях\n"
-                        f"/mdleth - Пишем команду указав сразу сумму желаймого обмена в леях\n"
+                        f"/mdlltc - Пишем команду указав сразу сумму обмена в леях MDL-LTC - '/mdlltc 500'\n"
+                        f"/mdlbtc - Пишем команду указав сразу сумму обмена в леях MDL-BTC\n"
+                        f"/mdleth - Пишем команду указав сразу сумму обмена в леях MDL-ETH\n"
                         f"/coinprices' - Актуальная цена популярных криптовалют"
                         )
+
 
 # Обрабатываю команду /rules
 @dp.message_handler(commands=['rules'])
 async def send_info(message: types.Message):
-    await message.reply(rulesmsg)
+    await message.reply(f"Строго запрещено в общем чате Цитадели:\n\n"
+                        f"{emoji.emojize(':camera_with_flash:')}Скидывать фотографии содержащие порнографию, насилие и фотографии местоположений запрещенных веществ.\n\n"
+                        f"{emoji.emojize(':hot_springs:')}Оскорблять администрацию или участников сообщества на не определенной основе /ban.\n\n"
+                        f"{emoji.emojize(':cross_mark:')}Обсуждение запрещенных веществ не предусмотренных сообществом.\n\n"
+                        f"{emoji.emojize(':double_exclamation_mark:')}Мы не приветствуем людей которые используют несколько аккаунтов для извлечения \n"
+                        f"выгоды в проводимых мероприятиях{emoji.emojize(':double_exclamation_mark:')}.")
+
 
 # Обрабатываю добавление нового пользователя в группу
 @dp.message_handler(content_types=types.ContentTypes.NEW_CHAT_MEMBERS)
@@ -66,15 +68,17 @@ async def new_chat_member(message: types.Message):
                              f"{emoji.emojize(':castle:')}Добро пожаловать в Цитадель{emoji.emojize(':castle:')}\n"
                              f"{emoji.emojize(':trident_emblem:')}Что бы узнать правила нашего сообщества воспользуйтесь командой /rules{emoji.emojize(':trident_emblem:')}")
 
+
 # Обрабатываю команду /coinprices
 @dp.message_handler(commands=['coinprices'])
 async def send_info(message: types.Message):
     await message.reply(f"Цена на BTC на текущий момент: ${str(btc_price)}\n"
-                        f"Что бы конвертировать MDL в BTC используй команду /mdlbtc '500'\n"
+                        f"Что бы конвертировать MDL в BTC используй команду /mdlbtc '500'\n\n"
                         f"Цена на LTC на текущий момент: ${str(ltc_price)}\n"
-                        f"Что бы конвертировать MDL в LTC используй команду /mdlltc '500'\n"
+                        f"Что бы конвертировать MDL в LTC используй команду /mdlltc '500'\n\n"
                         f"Цена на ETH на текущий момент: ${str(eth_price)}\n"
                         f"Что бы конвертировать MDL в ETH используй команду /mdleth '500'")
+
 
 @dp.message_handler(commands=['mdlltc'])
 async def convert_mdl(message: types.Message):
@@ -88,6 +92,7 @@ async def convert_mdl(message: types.Message):
         # Конвертируем получиные значения
         await message.reply(f'{number} MDL в LTC будет: {convert} LTC')
 
+
 @dp.message_handler(commands=['mdlbtc'])
 async def convert_mdl(message: types.Message):
     # Извлекаем число из сообщения с помощью регулярного выражения
@@ -99,6 +104,7 @@ async def convert_mdl(message: types.Message):
         convert = mdlbtc * number
         # Конвертируем получиные значения
         await message.reply(f'{number} MDL в BTC будет: {convert} BTC')
+
 
 @dp.message_handler(commands=['mdleth'])
 async def convert_mdl(message: types.Message):
