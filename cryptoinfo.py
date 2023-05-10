13c33381ba3d17338e6e4d28dfb41479f9575b7c
apt-get update <131296775+hobydy@users.noreply.github.com>
2023-05-10 16:18:09 +0300

    Update cryptoinfo.py

--- cryptoinfo.py
+++ cryptoinfo.py
@@ -20,8 +20,11 @@ eth_price = float(ETHUSDT['price'])
 urlusd = 'https://www.google.com/finance/quote/MDL-LTC?sa=X&ved=2ahUKEwjI-IGfu73-AhVKxAIHHSidC_0Q-fUHegQIBhAf'
 requsd = requests.get(urlusd)
 soup = BeautifulSoup(requsd.text, 'html.parser')
-mdlltcget = soup.find('div', class_="YMlKec fxKbKc").text
-mdlltc = float(mdlltcget)
+mdlltcget = soup.find('div', class_="YMlKec fxKbKc")
+if mdlltcget is not None:
+    mdlltc = float(mdlltcget.text)
+else:
+    print("Unable to find the element for MDL-LTC")
 
 # Достаем цену 1 лея в BTC
 urlbtc = "https://www.google.com/finance/quote/MDL-BTC?sa=X&ved=2ahUKEwjwmOeRib7-AhXy7LsIHQ0TCQEQ-fUHegQIBhAf"
@@ -30,9 +33,12 @@ soup = BeautifulSoup(reqbtc.text, "html.parser")
 mdlbtcget = soup.find('div', class_='YMlKec fxKbKc').text
 mdlbtc = float(mdlbtcget)
 
+
+
 # Достаем цену 1 лея в ETH
 urleth = 'https://www.google.com/finance/quote/MDL-ETH?sa=X&ved=2ahUKEwiL_faTjL7-AhUdgf0HHb54BksQ-fUHegQIDhAf'
 reqeth = requests.get(urleth)
 soup = BeautifulSoup(reqeth.text, "html.parser")
 mdlethget = soup.find('div', class_='YMlKec fxKbKc').text
 mdleth = float(mdlethget)
+
